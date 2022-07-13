13d9ff140a38759a956ff69acad5f915eeb8fb54
AcChrome <ac.chan@live.com>
2022-07-12 17:30:06 -0700

    hero component initial commit

---
+++ src/component/Hero.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Grid from "../images/photo-grid.png";
+
+export default function Hero() {
+  return (
+    <section>
+      <img src={Grid} className="" />
+      <h1> Online Experience</h1>
+      <p>
+        Join unique interaction activities led by one-of-a-kind hosts-all
+        without leaving home.
+      </p>
+    </section>
+  );
+}
