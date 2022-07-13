d56f881242fb200dbb646e1548c90569cc685238
AcChrome <ac.chan@live.com>
2022-07-12 17:53:13 -0700

    updated styling hero component

--- src/component/Hero.js
+++ src/component/Hero.js
@@ -4,9 +4,9 @@ import Grid from "../images/photo-grid.png";
 export default function Hero() {
   return (
     <section>
-      <img src={Grid} className="" />
-      <h1> Online Experience</h1>
-      <p>
+      <img src={Grid} className="hero--photo" />
+      <h1 className="hero--header"> Online Experience</h1>
+      <p className="hero--text">
         Join unique interaction activities led by one-of-a-kind hosts-all
         without leaving home.
       </p>
