8c4f2a341e8a89d43683090bfa257b777011c353
AcChrome <ac.chan@live.com>
2022-07-15 23:53:16 -0700

    finish styling card

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,10 +1,21 @@
 import React from "react";
 import pic from "../images/katie-zaferes.png";
+import star from "../images/star.png";
 
 export default function Card() {
   return (
     <div className="card">
-      <img src={pic}></img>
+      <img src={pic} className="card--image"></img>
+      <div className="card--stats">
+        <img src={star} className="card--star" />
+        <span>5.0</span>
+        <span className="gray">(6) . </span>
+        <span className="gray">USA</span>
+        <p>Life Lessons with Katie Zaferes</p>
+        <p>
+          <span className="bold">From $136 / person</span>
+        </p>
+      </div>
     </div>
   );
 }
