a1053a4a3371617531ffe89bef12316e8ad4fff2
AcChrome <ac.chan@live.com>
2022-08-26 23:53:39 -0700

    pass object as props

--- src/App.js
+++ src/App.js
@@ -6,19 +6,9 @@ import data from "./data";
 
 export default function App() {
   const cards = data.map((item) => {
-    return (
-      <Card
-        key={item.id}
-        img={item.coverImg}
-        rating={item.stats.rating}
-        reviewCount={item.stats.reviewCount}
-        location={item.location}
-        title={item.title}
-        price={item.price}
-        openSpots={item.openSpots}
-      />
-    );
+    return <Card key={item.id} item={item} />;
   });
+
   return (
     <div>
       <Navbar />
